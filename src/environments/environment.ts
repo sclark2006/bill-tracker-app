0e68ee4a38b49130b54dad80c89382a8f6af35e1
Frederick Clark <sclark2006@gmail.com>
2021-02-20 11:31:53 -0400

    fix: Remove comments from environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,22 +10,8 @@ export const environment = {
     messagingSenderId: "14338255113",
     appId: "1:14338255113:web:25c087499fc1352ccf4bd8",
     measurementId: "G-7WGLDG9PL8"
-}
-
-
-/** export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyCATun5z-FQAmxngFUWl0dkG2zmcn587B0',
-    authDomain: 'ionic-angular-event-manager.firebaseapp.com',
-    databaseURL: 'https://ionic-angular-event-manager.firebaseio.com',
-    projectId: 'ionic-angular-event-manager',
-    storageBucket: 'ionic-angular-event-manager.appspot.com',
-    messagingSenderId: '1055126399542',
-    appId: '1:1055126399542:web:3c36a5c070d8fd78'
   }
-};
-*/
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
