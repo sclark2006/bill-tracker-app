1cb9260bcf48bf45ddeecb9de97373fa080a737e
Frederick Clark <sclark2006@gmail.com>
2021-02-20 16:15:08 -0400

    feat: Bill Details page

--- src/app/pages/bill-detail/bill-detail.page.ts
+++ src/app/pages/bill-detail/bill-detail.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ActionSheetController, AlertController } from '@ionic/angular';
+import { BillService } from '../../services/bill.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-bill-detail',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BillDetailPage implements OnInit {
 
-  constructor() { }
+  public bill: Observable<any>;
+  public billId: string;
 
-  ngOnInit() {
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+    public actionCtrl: ActionSheetController,
+    public alertCtrl: AlertController,
+    public billService: BillService
+    ) { }
+
+  async ngOnInit() {
+    this.billId = this.route.snapshot.paramMap.get('id');
+    const bill$ = await this.billService.getBill(this.billId);
+    this.bill = bill$.valueChanges();
+  }
+
+  async showOptions(): Promise<void> {
+    const action = await this.actionCtrl.create({
+      header: 'Modify your bill',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.billService.removeBill(this.billId).then(() => {
+              this.router.navigateByUrl('home');
+            });
+          },
+        },
+        {
+          text: 'Mark as Paid!',
+          icon: 'checkmark',
+          handler: () => {
+            this.billService.payBill(this.billId);
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    await action.present();
   }
 
 }
